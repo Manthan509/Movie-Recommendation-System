5daae85822bf7ee2e194103754701ec09670b581
Manthan Mathur <mathurmanthan893@gmail.com>
2026-06-14 23:07:48 +0530

    Fix TMDB timeout and missing fields

--- tmdb.py
+++ tmdb.py
@@ -1,7 +1,8 @@
-import requests
+\import requests
 import re
 
 API_KEY = "7a10124c43a357ddce834f70877e3804"
+
 
 def fetch_movie(movie_name):
 
@@ -12,26 +13,48 @@ def fetch_movie(movie_name):
         f"?api_key={API_KEY}&query={clean_name}"
     )
 
-    response = requests.get(url)
+    try:
+        response = requests.get(url, timeout=10)
+        data = response.json()
 
-    data = response.json()
+    except requests.exceptions.RequestException:
+
+        return {
+            "title": movie_name,
+            "year": "N/A",
+            "rating": "N/A",
+            "plot": "Could not fetch details.",
+            "poster": "N/A"
+        }
 
     if len(data["results"]) == 0:
-        return None
+
+        return {
+            "title": movie_name,
+            "year": "N/A",
+            "rating": "N/A",
+            "plot": "Movie details not found.",
+            "poster": "N/A"
+        }
 
     movie = data["results"][0]
 
     poster = (
-        "https://image.tmdb.org/t/p/w500"
-        + movie["poster_path"]
+        "https://image.tmdb.org/t/p/w500" + movie["poster_path"]
         if movie["poster_path"]
-        else None
+        else "N/A"
+    )
+
+    year = (
+        movie["release_date"][:4]
+        if movie.get("release_date")
+        else "N/A"
     )
 
     return {
-        "title": movie["title"],
-        "rating": movie["vote_average"],
-        "overview": movie["overview"],
-        "poster": poster,
-        "release_date": movie["release_date"]
+        "title": movie.get("title", movie_name),
+        "year": year,
+        "rating": movie.get("vote_average", "N/A"),
+        "plot": movie.get("overview", "N/A"),
+        "poster": poster
     }
